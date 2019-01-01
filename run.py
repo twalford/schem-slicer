c730e8992bbaa9831e30e96c8e2a51242afc98ae
Thomas Walford <20815153+twalford@users.noreply.github.com>
2019-01-01 17:05:04 +1100

    added CLI

--- run.py
+++ run.py
@@ -1,113 +1,213 @@
-#written by ilerm
+#Written by ilerm
 #28/12/2018
+
+#Made for splitting minecraft map art into rows.
+#When used with redstonehelpers generator, use axis x.
+#Only supports a specific set of textures. Add your own :)
+ 
+import sys
 import numpy
 import nbt
+
 from PIL import Image, ImageFont, ImageDraw
-
-
 from numpy import array, zeros, fromstring
 from io import StringIO
-'''
-size = (100, 100)
-canvas = Image.new('RGB',size,(200,200,255))
-draw = ImageDraw.Draw(canvas)
-draw.text((10, 10), "hello")
-canvas.show()
-'''
-filename = "test.schematic"
-infile = nbt.nbt.NBTFile(filename, 'rb')
 
-width = infile['Width'].value
-length = infile['Length'].value
-height = infile['Height'].value
+strHelp = """
+Usage: run.py path axis [options]
+  path:  The path to your schematic file
+  axis:  The axis to slice into images. Must be x, y, or z
+    x: Looking East. Image 0 is the west-most layer
+    y: Top down. Image 0 is the bottom layer
+    z: Looking North. Image 0 is the north-most layer
 
-##### Load textures #####
-i_clay = Image.open('./res/clay.png')
-i_black_wool = Image.open('./res/black_wool.png')
-i_gray_clay = Image.open('./res/gray_clay.png')
-i_cyan_clay = Image.open('./res/cyan_clay.png')
-i_gray_wool = Image.open('./res/gray_wool.png')
-i_lgray_wool = Image.open('./res/lgray_wool.png')
-i_snow = Image.open('./res/snow.png')
-i_diorite = Image.open('./res/diorite.png')
-i_stone = Image.open('./res/stone.png')
-i_diamond = Image.open('./res/diamond.png')
+options:
+  -c  Crop out the air at the top of the image
+  -f  Flip the image left to right
+  -n  Write the slice number on the image
+  -g  Draw a grid (grey)
+  -bg 0-255 0-255 0-255   Set the background colour (default blue-grey)
 
-textures = [i_clay, #0
-i_black_wool, 		#1
-i_gray_clay ,		#2
-i_cyan_clay ,		#3
-i_gray_wool ,		#4
-i_lgray_wool,		#5
-i_snow,				#6
-i_diorite,			#7
-i_stone,			#8
-i_diamond]			#9
+examples:
+  run.py test.schematic x -n -f -c
+  run.py test.schematic y -n -bg 255 255 255
+  run.py test.schematic z -g"""
 
-sizeZ = 16*length
-sizeY = 16*height
-size = (sizeZ, sizeY)
-topY = 0
-hasBlockinRow = False
-
-##### Generate images #####
-for x in range(width):
-	if (x < 50):
-		continue
+def main():
+	##### Parse args #####
+	if len(sys.argv) <= 2:
+		print(strHelp)
+		return
 		
-	topY = 0
-	canvas = Image.new('RGB',size,(200,200,255))
+	doCrop = False
+	doFlip = False
+	doNumbering = False
+	doGrid = False
+	bg_R = 200
+	bg_G = 200
+	bg_B = 255
 	
-	for y in range(height):
-		hasBlockinRow = False
-		for z in range(length):
-			arrayPos = (y * length + z) * width + x
-			id = infile['Blocks'][arrayPos]
-			data = infile['Data'][arrayPos]
+	filename = sys.argv[1]
+	axis = sys.argv[2]
+	
+	if len(sys.argv) > 3:
+		for i in range(len(sys.argv)):
+			if sys.argv[i] == '-c':
+				doCrop = True
+			if sys.argv[i] == '-f':
+				doFlip = True
+			if sys.argv[i] == '-n':
+				doNumbering = True
+			if sys.argv[i] == '-g':
+				doGrid = True
+			if sys.argv[i] == '-bg':
+				bg_R = int(sys.argv[i+1])
+				bg_G = int(sys.argv[i+2])
+				bg_B = int(sys.argv[i+3])				
+	
+	##### Load schematic #####
+	infile = nbt.nbt.NBTFile(filename, 'rb')
+	
+	##### Load textures #####
+	textures = []
+	textures.insert(0, Image.open('./res/clay.png'))
+	textures.insert(1, Image.open('./res/black_wool.png'))
+	textures.insert(2, Image.open('./res/gray_clay.png'))
+	textures.insert(3, Image.open('./res/cyan_clay.png'))
+	textures.insert(4, Image.open('./res/gray_wool.png'))
+	textures.insert(5, Image.open('./res/lgray_wool.png'))
+	textures.insert(6, Image.open('./res/snow.png'))
+	textures.insert(7, Image.open('./res/diorite.png'))
+	textures.insert(8, Image.open('./res/stone.png'))
+	textures.insert(9, Image.open('./res/diamond.png'))
+	
+	##### Prepare loops#####
+	width = infile['Width'].value
+	length = infile['Length'].value
+	height = infile['Height'].value
+
+	imgW = 0 #pixel width of the images
+	imgH = 0 #pixel height of the images
+	size = (0,0) #pixel dimensions of the images 
+	
+	if axis == 'x':
+		outerRange = width
+		middleRange = height
+		innerRange = length
+	if axis == 'y':
+		outerRange = height
+		middleRange = length
+		innerRange = width
+	if axis == 'z':
+		outerRange = length
+		middleRange = height
+		innerRange = width
+	
+	imgW = 16 * innerRange
+	imgH = 16 * middleRange
+	size = (imgW, imgH)
+	
+	##### Display settings #####
+	print ("image width: {}".format(imgW))
+	print ("image height: {}".format(imgH))
+	print ("axis: {}".format(axis))
+	print ("do crop: {}".format(doCrop))
+	print ("do flip: {}".format(doFlip))
+	print ("do numbering: {}".format(doNumbering))
+	print ("do grid {}".format(doGrid))
+	print ("generating images...")
+	
+	##### Generate images #####
+	for outerLoop in range(outerRange):
+		
+		hightestRowNonAir = 0
+		canvas = Image.new('RGB',size,(bg_R,bg_G,bg_B))
+		
+		for middleLoop in range(middleRange):
 			
-			#print("{}, {}, {}".format(arrayPos, id, data))
-			if (id != 0):
-				hasBlockinRow = True
+			hasBlockinRow = False
+			for innerLoop in range(innerRange):
 				
-			if arrayPos == 1:
-				id = 0
-			if (id == 35): #wool
-				if (data == 15):#black wool
-					tex_index = 1
-				elif (data == 8):#light gray wool
-					tex_index = 5
-				elif (data == 7):#gray wool
-					tex_index = 4
-				elif (data == 0):#white wool
+				if axis == 'x':
+					arrayPos = (middleLoop * length + innerLoop) * width + outerLoop
+				if axis == 'y':
+					arrayPos = (outerLoop * length + middleLoop) * width + innerLoop
+				if axis == 'z':
+					arrayPos = (middleLoop * length + outerLoop) * width + innerLoop
+					
+				id = infile['Blocks'][arrayPos]
+				data = infile['Data'][arrayPos]
+				
+				if (id != 0):
+					hasBlockinRow = True
+				
+				#pre 1.13 id's
+				if (id == 35): #wool
+					if data == 15:#black wool
+						tex_index = 1
+					elif data == 8:#light gray wool
+						tex_index = 5
+					elif data == 7:#gray wool
+						tex_index = 4
+					elif data == 0:#white wool
+						tex_index = 6
+				elif id == 159:#terracotta
+					if data == 7:#gray terracotta
+						tex_index = 2
+					elif data == 9:#cyan terracotta
+						tex_index = 3
+				elif id == 1:#stone
+					tex_index = 8
+				elif id == 17:#birch log
+					tex_index = 7
+				elif id == 82:#clay
+					tex_index = 0
+				elif id == 57:#diamond
+					tex_index = 9
+				elif id == 20:#glass (show as dimaond)
+					tex_index = 9
+				elif id == 80:#snowblock
 					tex_index = 6
-			elif (id == 159):#terracotta
-				if (data == 7):#gray terracotta
-					tex_index = 2
-				elif(data == 9):#cyan terracotta
-					tex_index = 3
-			elif (id == 1):#stone
-				tex_index = 8
-			elif (id == 17):#birch log
-				tex_index = 7
-			elif (id == 82):#clay
-				tex_index = 0
-			elif (id == 57):#diamond
-				tex_index = 9
-			elif (id == 20):#glass
-				tex_index = 9
-			else:
-				continue
+				else:
+					continue
+				
+				if (axis == 'y'):
+					canvas.paste(textures[tex_index],(innerLoop*16, middleLoop*16)) #draws from top left (favour looking down for y axis)
+				else:
+					canvas.paste(textures[tex_index],(innerLoop*16, imgH - (middleLoop*16) - 16)) #draws from bottom left
+					
+				
+				#end inner loop
+				
+			if (hasBlockinRow == True):
+				hightestRowNonAir = middleLoop
+			#end middle loop
+		
+		### Options ###
+		if doCrop:
+			canvas = canvas.crop((0,imgH - hightestRowNonAir*16 - 16,imgW,imgH)) #Crops out the image above hightestRowNonAir.
+		if doFlip:
+			canvas = canvas.transpose(Image.FLIP_LEFT_RIGHT) #Flips the image left-to-right
 			
-			canvas.paste(textures[tex_index],(z*16, sizeY - (y*16) - 16, z*16 + 16, sizeY - (y*16)))
-		canvas.paste(textures[9],(sizeZ - 16,sizeY - (y*16) - 16,sizeZ,sizeY - (y*16)))
-			
-		if (hasBlockinRow == True):
-			topY = y
-	
-	draw = ImageDraw.Draw(canvas)
-	draw.text(((sizeZ) - 20, sizeY - 64), format(x))
-	ci = canvas.crop((0,sizeY - topY*16 - 16,sizeZ,sizeY))
-	ci.save('./out/{}.png'.format(x))
-	
-	print ("saved image {}".format(x))
-print ("finished")
+		draw = ImageDraw.Draw(canvas)
+		if doNumbering:
+			draw.text((2, imgH - 12), format(outerLoop)) #Draws the slice number in bottom left.
+		if doGrid: #Draws a grid
+			for i in range(innerRange):
+				draw.line((i*16, 0, i*16, imgH), fill=(150,150,150))
+			for j in range(middleRange):
+				draw.line((0, j*16, imgW+16, j*16), fill=(150,150,150))
+		
+		del draw
+		canvas.save("./out/{}.png".format(outerLoop)) #Saves the image
+		
+		print ("saved image {}".format(outerLoop))
+		#end outer loop
+		
+	print ("finished. Check the out folder!")
+
+if __name__ == '__main__':
+	try:
+		main()
+	except:
+		print(strHelp) # lol
